399c70313b30cb932f130ce1d455187bba8a7b78
Mukund Kidambi <mukkid@gmail.com>
2025-12-08 21:56:05 -0600

    d5p2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,6 +1,6 @@
 const INPUT_STR: &str = include_str!("input.txt");
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Range {
     lower: u64,
     upper: u64,
@@ -14,8 +14,8 @@ impl Range {
             .split_once("-")
             .map(|(n1, n2)| {
                 (
-                    n1.parse::<u64>().unwrap_or(0),
-                    n2.parse::<u64>().unwrap_or(0),
+                    n1.parse::<u64>().unwrap(),
+                    n2.parse::<u64>().unwrap(),
                 )
             })
             .unwrap();
@@ -32,7 +32,8 @@ impl Range {
 
 fn main() {
     let out1 = solve1(INPUT_STR);
-    println!("{out1}");
+    let out2 = solve2(INPUT_STR);
+    println!("{out1}, {out2}");
 }
 
 fn solve1(input: &str) -> u32 {
@@ -59,6 +60,47 @@ fn solve1(input: &str) -> u32 {
     count
 }
 
+fn solve2(input: &str) -> u64 {
+    let mut ranges: Vec<Range> = vec![];
+    let mut l_iter = input.lines();
+    while let Some(line) = l_iter.next() {
+        if line.is_empty() {
+            break
+        }
+        let r = Range::from_string(line);
+        ranges.push(r);
+    }
+    compress_ranges(ranges)
+}
+
+fn compress_ranges(ranges: Vec<Range>) -> u64 {
+    let mut range = ranges.clone();
+    'outer: loop {
+        for (idx, first) in (0..range.len() - 1).enumerate() {
+            for second in idx+1..range.len() {
+                if range[first].contains(range[second].lower) {
+                    range[first].upper = range[first].upper.max(range[second].upper);
+                    range.remove(second);
+                    continue 'outer
+                } else if range[first].contains(range[second].upper) {
+                    range[first].lower = range[first].lower.min(range[second].lower);
+                    range.remove(second);
+                    continue 'outer
+                } else if range[second].contains(range[first].lower) {
+                    range[second].upper = range[second].upper.max(range[first].upper);
+                    range.remove(first);
+                    continue 'outer
+                } else if range[second].contains(range[first].upper) {
+                    range[second].lower = range[second].lower.min(range[first].lower);
+                    range.remove(first);
+                    continue 'outer
+                }
+            }
+        }
+        return range.into_iter().map(|r| (r.upper - r.lower) + 1).sum::<u64>()
+    }    
+}
+
 #[cfg(test)]
 mod test {
     const TEST_STR: &str = include_str!("test.txt");
@@ -66,5 +108,11 @@ mod test {
     fn part1() {
         let out = crate::solve1(TEST_STR);
         assert_eq!(out, 3)
+    }
+
+    #[test]
+    fn part2() {
+        let out = crate::solve2(TEST_STR);
+        assert_eq!(out, 14);
     }
 }
