fda302301ee9d13938ffb415f65ae47ba265729a
Mukund Kidambi <mukkid@gmail.com>
2025-12-21 01:37:22 -0600

    day10p2 initial structs

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,18 +1,26 @@
-use std::fmt;
 use regex::Regex;
+use std::fmt;
 
 const INPUT_STR: &str = include_str!("input.txt");
 
 fn main() {
-    let out1 = solve1(INPUT_STR);
+    let (out1, out2) = solve1(INPUT_STR);
     println!("{out1}");
+    println!("{out2}");
 }
 
+#[derive(Debug)]
 struct LightMachine {
     len: usize,
     target_light: u16,
     current_light: u16,
-    buttons: Vec<u16>
+    buttons: Vec<u16>,
+}
+
+#[derive(Debug)]
+struct JoltMachine {
+    joltages: Vec<usize>,
+    buttons: Vec<Vec<usize>>
 }
 
 // definitions:
@@ -21,14 +29,13 @@ struct LightMachine {
 // 0b 00010 -> press the second button
 // 0b 00101 -> press the first and third button
 
-
 impl LightMachine {
     fn press_buttons(&self) -> usize {
         let limit = 2_i32.pow(self.buttons.len() as u32);
         let mut fewest_presses = self.buttons.len();
         for press in 0..limit {
             let mut button_press = 0;
-            for idx in 0..=self.len+1 {
+            for idx in 0..=self.len + 1 {
                 if press >> idx & 0x1 == 0x1 {
                     button_press ^= self.buttons[idx];
                 }
@@ -44,42 +51,51 @@ impl LightMachine {
     }
 }
 
-impl fmt::Debug for LightMachine {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let _ = write!(f, "len: {}\n", self.len);
-        let _ = write!(f, "target_light: {:0len$b}\n", self.target_light.reverse_bits() >> 16-self.len, len = self.len);
-        let _ = write!(f, "current_light: {:0len$b}\n", self.current_light.reverse_bits() >> 16-self.len, len = self.len);
-        let _ = write!(f, "buttons: {{\n");
-        for b in &self.buttons {
-            let _ = write!(f, "\t{:0len$b}\n", b.reverse_bits() >> 16-self.len, len = self.len);
-        }
-        write!(f, "}}")
-    }
-}
-
-fn solve1(input_str: &str) -> usize {
+fn solve1(input_str: &str) -> (usize, usize) {
     let re = Regex::new(r"\[([^\]]+)\] (\([^{]+) \{([^}]+)\}").unwrap();
     let mut lms = vec![];
-    for (_, [lights, buttons, _]) in re.captures_iter(input_str).map(|c| c.extract()) {
+    let mut jms = vec![];
+    for (_, [lights, buttons, joltages]) in re.captures_iter(input_str).map(|c| c.extract()) {
         // reversed because the 0th digit is the leftmost one
-        let l = lights.chars().rev().fold(0b0, |acc, c| {
-            if c == '.' {
-                acc << 1
-            } else {
-                (acc | 0b1) << 1
-            }
-        }) >> 1;
+        let l = lights.chars().rev().fold(
+            0b0,
+            |acc, c| {
+                if c == '.' { acc << 1 } else { (acc | 0b1) << 1 }
+            },
+        ) >> 1;
 
-        let b: Vec<u16> = buttons.split_whitespace().map(|b| {
-            b[1..b.len()-1].split(',').fold(0_u16, |acc, bm| {
-                acc | 1 << bm.parse::<u8>().unwrap()
+        let jb: Vec<Vec<usize>> = buttons
+            .split_whitespace()
+            .map(|b| b[1..b.len() - 1].split(",").map(|i| i.parse().unwrap()).collect())
+            .collect();
+
+        let b: Vec<u16> = buttons
+            .split_whitespace()
+            .map(|b| {
+                b[1..b.len() - 1]
+                    .split(',')
+                    .fold(0_u16, |acc, bm| acc | 1 << bm.parse::<u8>().unwrap())
             })
-                
-        }).collect();
-        let lm = LightMachine { len: lights.len(), target_light: l, current_light: 0, buttons: b };
+            .collect();
+
+        let j: Vec<usize> = joltages.split(",").map(|c| c.parse().unwrap()).collect();
+        let lm = LightMachine {
+            len: lights.len(),
+            target_light: l,
+            current_light: 0,
+            buttons: b,
+        };
+
+        let jm = JoltMachine {
+            joltages: j,
+            buttons: jb,
+        };
         lms.push(lm);
+        jms.push(jm);
     }
-    lms.iter().map(|lm| lm.press_buttons()).sum::<usize>()
+    let lights = lms.iter().map(|lm| lm.press_buttons()).sum::<usize>();
+    dbg!(jms);
+    (lights, 0)
 }
 
 #[cfg(test)]
@@ -88,7 +104,13 @@ mod test {
 
     #[test]
     fn part1() {
-        let out = crate::solve1(TEST_STR);
-        assert_eq!(out, 7);
+        let (out1, _) = crate::solve1(TEST_STR);
+        assert_eq!(out1, 7);
+    }
+
+    #[test]
+    fn part2() {
+        let (_, out2) = crate::solve1(TEST_STR);
+        assert_eq!(out2, 33);
     }
 }
